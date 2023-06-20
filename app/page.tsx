c1797328c81182a6dad535d2fd05ecaecf65be25
Steven <58159084+Auxdible@users.noreply.github.com>
2023-06-19 23:15:47 -0400

    feat: in-app browser support

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
     <header className="flex flex-row max-md:flex-col min-h-screen w-full items-center mx-auto max-md:my-16">
       <div className={"flex flex-col content-center gap-5 flex-1 flex-grow font-roboto text-2xl text"}>
           <section className={"max-md:text-center mx-auto"}>
-          <h1 className={"text-8xl pt-4 font-montserrat text-primary"}>Auxdible</h1>
-          <p className={"text-4xl font-montserrat py-2"}>Full Stack Developer</p>
+          <h1 className={"text-8xl max-sm:text-5xl pt-4 font-montserrat text-primary"}>Auxdible</h1>
+          <p className={"text-4xl max-sm:text-2xl font-montserrat py-2"}>Full Stack Developer</p>
           </section>
-          <button onClick={() => changeGradient() } className={"mx-auto w-fit border italic dark:border-orange-400 border-orange-700 p-1 rounded-lg"}>Change glasses gradient 😎</button>
+          <button onClick={() => changeGradient() } className={"mx-auto w-fit border italic max-sm:text-xl dark:border-orange-400 border-orange-700 p-1 rounded-lg"}>Change glasses gradient 😎</button>
       </div>
       <div className={"flex flex-1 flex-grow flex-shrink max-md:w-screen md:h-screen"}>
         <div className={"w-full"}>
