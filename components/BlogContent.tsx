df125a6fbd0ef6b1d3aff464cb141d0aa5da3a3f
Steven <58159084+Auxdible@users.noreply.github.com>
2023-08-19 18:26:20 -0400

    fix: blog content bein wack

--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -16,7 +16,7 @@ export default function BlogContent({ post, preview }: PostProps) {
         <p className={"block w-full font-roboto text-2xl my-4 break-words"}>{post.post_description}</p>
         <p className={"flex flex-row font-roboto justify-center text-xl dark:text-gray-400 text-gray-600 my-1 gap-1"}>{new Date(post.post_date_unix || Date.now()).toISOString().split('T')[0]} • <span className={"flex flex-row justify-center items-center gap-1"}><BsShieldCheck/> {post.posted_by}</span></p>
     </div>
-    <div className={"markdown block mx-auto border-2 dark:border-orange-400 border-orange-700 rounded-3xl md:p-4 max-md:py-4 p-1 w-fit my-5"} dangerouslySetInnerHTML={{ __html: converter.makeHtml(post.post_content) }}>   
+    <div className={"markdown block mx-auto border-2 dark:border-orange-400 border-orange-700 rounded-3xl md:p-4 max-md:py-4 p-1 max-md:w-screen my-5"} dangerouslySetInnerHTML={{ __html: converter.makeHtml(post.post_content) }}>   
     </div>
     {!preview ? <span className={"flex flex-col max-w-lg w-full justify-between align-middle items-center gap-4"}>
         <Reactions post={post as any}/>
