c41198b92810c8c086a2fde64d7c45a6f8273927
Steven <58159084+Auxdible@users.noreply.github.com>
2024-07-03 07:46:28 -0400

    feat(blog): begin implementation of blog content

--- app/blog/[postId]/page.tsx
+++ app/blog/[postId]/page.tsx
@@ -1,16 +1,13 @@
-"use client";
-
 import NotFound from "@/app/not-found";
 import BlogContent from "@/components/blog/posts/BlogContent";
 import Loading from "@/components/Loading";
+import { getPostContent } from "@/lib/clients/s3";
 import { useRouter } from "next/navigation";
 import { useQuery } from "react-query";
 type PostProps = { params: { postId: string } };
-export default function BlogPost({ params: { postId } }: PostProps) {
-    const {data: post, status } = useQuery(["post_fetch"], async () => await fetch(`/api/public/posts?postId=${postId}`).then(async (data) => await data.json().catch(() => null)).catch(() => null))
-    if (status == 'loading') return <Loading/>;
-    if (status == 'success' && !post?.length) return <NotFound/> 
+export default async function BlogPost({ params: { postId } }: PostProps) {
+    const post = await getPostContent(postId)
     return (<>
-    {post && post[0] ? <BlogContent post={post[0]}/> : <main className={"flex min-h-screen flex-col items-center justify-center max-w-lg mx-auto"}><h1 className={"text-5xl max-sm:text-4xl font-montserrat text-title text-center my-3"}>Post couldn&apos;t be found.</h1></main>}
+    {post ? <BlogContent post={post}/> : <main className={"flex min-h-screen flex-col items-center justify-center max-w-lg mx-auto"}><h1 className={"text-5xl max-sm:text-4xl font-montserrat text-title text-center my-3"}>Post couldn&apos;t be found.</h1></main>}
     </>)
 }
