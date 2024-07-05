0aeb970e6d3422ee1e6794ba58f3233687f78788
Steven <58159084+Auxdible@users.noreply.github.com>
2024-07-05 13:02:23 -0400

    delete: delete dashboard and authentication for now, will return to when blog is added fully

--- lib/authenticateRoute.ts
+++
@@ -1,8 +0,0 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-
-export default async function authenticateRoute() {
-    const session = await getServerSession(authOptions);
-    if (!session?.user?.admin) return { "error": "You are not authorized to use this!" };
-    return undefined;
-}
