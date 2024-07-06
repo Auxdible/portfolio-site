6acedb4bb61d001a656e7f68e0b51aa7701ce92d
Steven <58159084+Auxdible@users.noreply.github.com>
2024-07-06 10:49:25 -0400

    chore(blog): delete old endpoint from layout, maybe that's vercels issue

--- app/blog/[postId]/page.tsx
+++ app/blog/[postId]/page.tsx
@@ -3,7 +3,7 @@ import { getPostContent } from "@/lib/clients/s3";
 
 type PostProps = { params: { postId: string } };
 export default async function BlogPost({ params: { postId } }: PostProps) {
-    const post = await getPostContent(postId)
+    const post = await getPostContent(postId);
     return (<>
     {post ? <BlogContent post={post}/> : <main className={"flex min-h-screen flex-col items-center justify-center max-w-lg mx-auto"}><h1 className={"text-5xl max-sm:text-4xl font-montserrat text-title text-center my-3"}>Post couldn&apos;t be found.</h1></main>}
     </>)
