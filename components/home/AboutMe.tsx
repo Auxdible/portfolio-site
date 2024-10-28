5cf63a81ca10591841ef29b5d1e9a909362c516d
Steven <58159084+Auxdible@users.noreply.github.com>
2024-10-28 17:37:55 -0400

    feat(page): add cv/resume to navbar

--- components/home/AboutMe.tsx
+++ components/home/AboutMe.tsx
@@ -4,22 +4,28 @@ import { Jobs } from './Jobs';
 import { Button } from '../ui/Button';
 import { SiNextdotjs, SiReact } from 'react-icons/si';
 import { dancingScript, sourceCodePro } from '@/app/fonts';
+import { Marquee } from '../ui/marquee';
 export default function AboutMe() {
     return (<section className={`gap-10 flex flex-col items-center relative ${sourceCodePro.variable} ${dancingScript.variable}`}>
 
-      <motion.div className='text-4xl max-sm:text-3xl max-w-6xl line-clamp-5 mx-auto font-lato leading-[3.5rem] flex flex-col items-center gap-10'>
-        <span className='text-center'>Hello! My name is <span className='text-title font-raleway'>Steven Primeaux</span>, a Full Stack Developer with three years of experience developing web-based applications using <span className="inline-flex items-center gap-2 align-bottom"><SiNextdotjs/> Next.js</span> and the <span className="inline-flex items-center gap-2 align-bottom"><SiReact/> MERN</span> tech stack.</span>
-        <span className='flex flex-col gap-2 items-center mx-auto text-center leading-none'><span>I 
-          <span className="font-cursive"> design</span> and 
+      <motion.div className='mx-auto w-full'>
+        <div className='max-w-6xl font-lato leading-[3.5rem] text-4xl max-sm:text-3xl line-clamp-5 flex flex-col items-center gap-10 mx-auto mb-6'>
+          <span className='text-center'>Hello! My name is <span className='text-title font-raleway'>Steven Primeaux</span>, a Full Stack Developer with three years of experience developing web-based applications using <span className="inline-flex items-center gap-2 align-bottom"><SiNextdotjs/> Next.js</span> and the <span className="inline-flex items-center gap-2 align-bottom"><SiReact/> MERN</span> tech stack.</span>
+          <span className='flex flex-col gap-2 items-center mx-auto text-center leading-none'><span>I 
+            <span className="font-cursive"> design</span> and 
           <span className="font-code text-gray-900 p-1 dark:text-gray-200  leading-none">&lt;develop/&gt;</span>
-        </span><Jobs/></span></motion.div>
-        <span className='flex w-full justify-between max-w-xl max-lg:flex-col max-lg:gap-10 max-lg:items-center'>
-        <motion.div>
-          <Button href='/contact-me' className='text-2xl font-bold'>Contact Me</Button>
-        </motion.div>
-        <motion.div>
-          <Button target='_blank' href={process.env.NEXT_PUBLIC_CV_URL} className='text-2xl font-bold'>My CV</Button>
-        </motion.div>
         </span>
+        
+          </span>
+        </div>
+        <Marquee className={'header text-5xl max-md:text-3xl text-title font-raleway font-bold overflow-y-hidden gap-16 w-fit min-w-0 pr-16 bg-gradient-conic  pb-2'}>
+          <span className='whitespace-nowrap'>Discord Bots</span>
+          <span className='whitespace-nowrap'>Cloud Applications</span>
+          <span className='whitespace-nowrap'>Mods</span>
+          <span className='whitespace-nowrap'>Professional Websites</span>
+          <span className='whitespace-nowrap'>Plugins</span>
+          <span className='whitespace-nowrap'>Community Projects</span>
+        </Marquee>
+        </motion.div>
       </section>)
 }
